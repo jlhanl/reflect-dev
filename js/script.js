1c13f2b88cfe428f0a3c5e98037a0a60d4a4c3dd
Jenny <jennylhanlon@gmail.com>
2016-02-09 16:35:33 -0800

    Added feature hover image interactivity

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//jQuery
+
+$( document ).ready(function() {
+
+  $('.feature').mouseover(function() {
+    $(this).css('border-left', '3px solid #F8E450');
+  });
+
+  $('.feature').mouseout(function() {
+    $(this).css('border-left', 'none');
+  });
+
+
+
+  $('#search').mouseover(function() {
+    $("#searchimg").attr('src', '../images/searchfeat.png');
+  });
+
+  $('#seek').mouseover(function() {
+    $("#searchimg").attr('src', '../images/seek.png');
+  });
+
+  $('#volunteer').mouseover(function() {
+    $("#searchimg").attr('src', '../images/volunteer.png');
+  });
+
+});
